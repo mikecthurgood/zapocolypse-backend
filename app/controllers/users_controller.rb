48ea6d4a985f2c5df98f3263e6d8afa5344055d2
github username <github email address>
2019-11-07 15:56:41 +0000

    sign in and validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,21 @@ class UsersController < ApplicationController
     user = User.find_by(username: params[:username])
 
     if user && user.authenticate(params[:password])
-      render json: user
+      render json: {username: user.username, token: issue_token({id: user.id})}
     else
       render json: {error: 'Username or Password is invalid.'}, status: 401
     end
 
+  end
+
+  def validate
+    user = get_current_user
+    
+    if user
+      render json: {username: user.username, token: issue_token({id: user.id})}
+    else
+      render json: {error: 'Unable to validate user.'}, status: 401
+    end
   end
   
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-
-  # get 'skills/index'
-  # get 'skills/show'
-  # get 'skills/update'
-  # get 'users/index'
-
-  # get 'users/show'
-  # get 'users/create'
-  # get 'users/update'
-  # get 'users/delete'
-
   post '/signin', to: 'users#signin'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/validate', to: 'users#validate'
 end
