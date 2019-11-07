4026110e81533bffe11bcd818202534e590191cf
github username <github email address>
2019-11-07 13:53:04 +0000

    added jwt gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'skills/index'
-  get 'skills/show'
-  get 'skills/update'
-  get 'users/index'
+  # get 'skills/index'
+  # get 'skills/show'
+  # get 'skills/update'
+  # get 'users/index'
 
-  get 'users/show'
-  get 'users/create'
-  get 'users/update'
-  get 'users/delete'
+  # get 'users/show'
+  # get 'users/create'
+  # get 'users/update'
+  # get 'users/delete'
 
-  # post '/signin', to 'users#signin'
+  post '/signin', to: 'users#signin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
