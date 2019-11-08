9b1c21758103f69ed1f5b008441abe995160b620
github username <github email address>
2019-11-08 12:39:46 +0000

    added user skills to API resp of signin/validate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     user = User.find_by(username: params[:username])
 
     if user && user.authenticate(params[:password])
-      render json: {username: user.username, token: issue_token({id: user.id})}
+      render json: {username: user.username, userskills: user.skills, token: issue_token({id: user.id})}
     else
       render json: {error: 'Username or Password is invalid.'}, status: 401
     end
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     user = get_current_user
     
     if user
-      render json: {username: user.username, token: issue_token({id: user.id})}
+      render json: {username: user.username, userskills: user.skills, token: issue_token({id: user.id})}
     else
       render json: {error: 'Unable to validate user.'}, status: 401
     end
