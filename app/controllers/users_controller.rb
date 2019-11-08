f858aaae1bbb6a11b4cc2b3b13339b71c36744bb
github username <github email address>
2019-11-08 19:02:59 +0000

    (most likely) deprecated use of userskill jointabl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   
   def signin
     user = User.find_by(username: params[:username])
-
+    
     if user && user.authenticate(params[:password])
-      render json: {username: user.username, userskills: user.skills, token: issue_token({id: user.id})}
+      render json: {username: user.username, userSkills: user.skills, userActivities: user.activities, token: issue_token({id: user.id})}
     else
       render json: {error: 'Username or Password is invalid.'}, status: 401
     end
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     user = get_current_user
     
     if user
-      render json: {username: user.username, userskills: user.skills, token: issue_token({id: user.id})}
+      render json: {username: user.username, userSkills: user.skills, userActivities: user.activities, token: issue_token({id: user.id})}
     else
       render json: {error: 'Unable to validate user.'}, status: 401
     end
