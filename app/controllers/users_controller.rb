d62bda62916a20c742cd9c7036eb6ccba6f8b75a
github username <github email address>
2019-11-11 14:15:33 +0000

    added user serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,11 @@ class UsersController < ApplicationController
   
   def signin
     user = User.find_by(username: params[:username])
-    
 
     if user && user.authenticate(params[:password])
-      render json: {username: user.username, userSkills: user.skills, userActivities: user.activities, userSkillZaps: user.skill_zaps, token: issue_token({id: user.id})}
+      token = issue_token({id: user.id})
+      render json: UserSerializer.new(user, token).to_serialized_json
+      #{username: user.username, userSkills: user.skills, userActivities: user.activities, userSkillZaps: user.skill_zaps, token: issue_token({id: user.id})}
     else
       render json: {error: 'Username or Password is invalid.'}, status: 401
     end
@@ -14,10 +15,11 @@ class UsersController < ApplicationController
 
   def validate
     user = get_current_user
-    
 
     if user
-      render json: {username: user.username, userSkills: user.skills, userActivities: user.activities, userSkillZaps: user.skill_zaps, token: issue_token({id: user.id})}
+      token = issue_token({id: user.id})
+      render json: UserSerializer.new(user, token).to_serialized_json
+      #{username: user.username, userSkills: user.skills, userActivities: user.activities, userSkillZaps: user.skill_zaps, token: issue_token({id: user.id})}
     else
       render json: {error: 'Unable to validate user.'}, status: 401
     end
@@ -27,7 +29,9 @@ class UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
-      render json: {username: user.username, userSkills: user.skills, userActivities: user.activities, userSkillZaps: user.skill_zaps, token: issue_token({id: user.id})}
+      token = issue_token({id: user.id})
+      render json: UserSerializer.new(user, token).to_serialized_json
+      #{username: user.username, userSkills: user.skills, userActivities: user.activities, userSkillZaps: user.skill_zaps, token: issue_token({id: user.id})}
     else 
       render json: {error: 'Unable to create user.'}, status: 500
     end
