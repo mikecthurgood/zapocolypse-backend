3482514f36c65de0747afa3dfb9e95aebd3cbe18
Mike Thurgood <mikecthurgood@gmail.com>
2019-11-12 20:21:28 +0000

    activity booking added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     if user && user.authenticate(params[:password])
       token = issue_token({id: user.id})
       render json: { token: token, user: UserSerializer.new(user), skillZaps: user.skill_zaps, userSkills: user.skills }
-      #{username: user.username, userSkills: user.skills, userActivities: user.activities, userSkillZaps: user.skill_zaps, token: issue_token({id: user.id})}
+      # {username: user.username, userSkills: user.skills, userActivities: user.activities, userSkillZaps: user.skill_zaps, token: issue_token({id: user.id})}
     else
       render json: {error: 'Username or Password is invalid.'}, status: 401
     end
